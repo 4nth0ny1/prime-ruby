6ded2f991799e425e80c8a5dc0724d1f1bfc7f57
4anth0ny1 <acatullo4@gmail.com>
2020-12-14 23:38:48 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+   if number <= 1 || number == 0 || number == 1
+    false
+   else
+    x = 2
+    while x < number
+      return false if number % x == 0
+      x = x + 1
+    end
+    true
+  end
+end
