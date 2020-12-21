f6871cff04fa41b01cdf5263ba909be5e1084fc7
4anth0ny1 <acatullo4@gmail.com>
2020-12-20 19:23:36 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
 def prime?(number)
-   if number <= 1 
-    false
-   else
-    x = 2
-    while x < number
-      return false if number % x == 0
-      x = x + 1
-    end
-    true
-  end
+  i = 2 
+  while i < number 
+    return false if number % i == 0 
+    i = i + 1
+  end 
+  number > 1 ? true : false
 end
